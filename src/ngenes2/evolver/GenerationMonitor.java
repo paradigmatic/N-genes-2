784a799541c2dfe717b0bca756a2a3d5cbb295ea
JL Falcone <jean-luc.falcone@unige.ch>
2010-04-07 15:45:53 +0200

    Moved monitors in their own package

--- src/ngenes2/evolver/GenerationMonitor.java
+++
@@ -1,9 +0,0 @@
-package ngenes2.evolver;
-
-import ngenes2.individual.Individual;
-import ngenes2.population.Population;
-
-
-public interface GenerationMonitor<G,I extends Individual<G,I>> {
-    public void newGeneration( int generationNumber, Population<G,I> pop);
-}
